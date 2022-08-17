3e52d30931de04ee054db6bdac77cfec26b0bda1
oyiakoumis <odysseasyiakoumis@gmail.com>
2022-08-18 00:04:11 +0200

    add Weird Algorithm

---
+++ Introductory Problems/Weird Algorithm.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+  long long n;
+  cin >> n;
+  cout << n << endl;
+  while (n > 1) {
+    n = (n % 2 == 0 ? n / 2 : 3 * n + 1);
+    cout << n << endl;
+  }
+}
+
+int main() {
+  solve();
+
+  return 0;
+}
